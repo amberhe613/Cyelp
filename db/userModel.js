c494347194ca035d930b87e5abfefd85ea3f8020
fayeoyaee <ouyang.f@husky.neu.edu>
2018-03-28 18:48:31 -0700

    db template

---
+++ db/userModel.js
@@ -0,0 +1,21 @@
+// may follow the codes below to create userModel
+/*
+let mongoose = require('mongoose');
+
+var productSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    img: {
+        type: String,
+        required: true
+    },
+});
+
+module.exports = mongoose.model('product', productSchema);
+*/
