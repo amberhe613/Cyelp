8c676541939d6a4811c4ec18765c7275e4389f88
Yujia Lu <yujia_lu@outlook.com>
2018-04-01 23:35:50 -0700

    update review route

--- routes/reviewRoute.js
+++ routes/reviewRoute.js
@@ -8,10 +8,18 @@ router.post('/restaurants/:restaurantId/reviews', function (req, res) {
     Restaurant.findById(req.params.restaurantId, function(err, restaurant) {
       if (err) {
           console.log(err);
+          res.status(400);
+          res.json({
+              "error": "error while creating review"
+          });
       } else {
           Review.create(req.body.comment, function(err, review){
               if (err) {
                   console.log(err);
+                  res.status(400);
+                  res.json({
+                      "error": "error while creating review"
+                  });
               } else {
                   // add user id to review
                   review._author = req.user._id;
@@ -19,7 +27,7 @@ router.post('/restaurants/:restaurantId/reviews', function (req, res) {
                   review.save();
                   console.log(review);
                   res.status(201);
-                  res.json({message: "Successfully added comment"});
+                  res.json({message: "Successfully added review"});
               }
           })
       }
@@ -27,13 +35,35 @@ router.post('/restaurants/:restaurantId/reviews', function (req, res) {
 });
 
 // edit review
-router.put("/restaurants/:restaurantId/reviews/:reviewId/edit", function(req, res){
+router.put("/restaurants/:restaurantId/reviews/:reviewId", function(req, res){
     Review.findByIdAndUpdate(req.params.reviewId, req.body.review, function(err, updatedComment){
         if(err){
             console.log(err);
+            res.status(400);
+            res.json({
+                "error": "error while updating review"
+            });
         } else {
             res.status(200);
-            res.json({message: "Successfully updated comment"});
+            res.json({message: "Successfully updated review"});
         }
     });
 });
+
+// delete review
+router.delete("/restaurants/:restaurantId/reviews/:reviewId", function(req, res){
+    //findByIdAndRemove
+    Review.findByIdAndRemove(req.params.reviewId, function(err){
+        if(err){
+            res.status(400);
+            res.json({
+                "error": "error while deleting review"
+            });
+        } else {
+            res.status(200);
+            res.json({message: "Successfully deleted review"});
+        }
+    });
+});
+
+module.exports = router;
