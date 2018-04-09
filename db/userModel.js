21df1ac5b30b39b95b764f13a2c98c4cbd2e087d
Yujia Lu <yujia_lu@outlook.com>
2018-04-09 14:20:20 -0700

    update user model

--- db/userModel.js
+++ db/userModel.js
@@ -1,6 +1,4 @@
 const mongoose = require("mongoose");
-const passportLocalMongoose = require("passport-local-mongoose");
-const uniqueValidator = require('mongoose-unique-validator');
 
 // schema setup
 const UserSchema = new mongoose.Schema({
@@ -25,10 +23,5 @@ const UserSchema = new mongoose.Schema({
         ref: "Review"
     }]
 });
-
-UserSchema.plugin(uniqueValidator, {message: 'is already taken.'});
-// passport-Local Mongoose will add a username, hash and salt field to store the username, the hashed password and the
-// salt value
-UserSchema.plugin(passportLocalMongoose);
 
 module.exports = mongoose.model("User", UserSchema);
