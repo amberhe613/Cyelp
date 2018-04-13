9eb24daf4a204605caa0dcc7e825510ad8d9c11d
Yujia Lu <yujia_lu@outlook.com>
2018-04-12 23:39:38 -0700

    style new restaurant page

--- client/src/components/restaurant/view/newRestaurant.js
+++ client/src/components/restaurant/view/newRestaurant.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {checkLogin} from '../../user/userService';
 import {createRestaurant} from '../restaurantService';
-import {Navbar, NavbarBrand, NavItem, NavLink, Nav } from 'reactstrap';
+import {Navbar, NavbarBrand, NavItem, NavLink, Nav, Container, Button, Form, FormGroup, Input, Label } from 'reactstrap';
 
 export default class NewRestaurant extends React.Component {
     // need a props for userId
@@ -55,7 +55,7 @@ export default class NewRestaurant extends React.Component {
         if (this.state.isAuthenticated) {
             return (
                 <div>
-                    <Navbar color="light" light expand="md">
+                    <Navbar color="light" light expand="xs">
                         <NavbarBrand href="/restaurants">Cyelp</NavbarBrand>
                         <Nav className="ml-auto" navbar>
                             <NavItem>
@@ -66,38 +66,49 @@ export default class NewRestaurant extends React.Component {
                             </NavItem>
                         </Nav>
                     </Navbar>
-                    <div className="container">
-                        <form>
-                            <p>
-                                Restaurant Name {' '}
-                                <input
+                    <Container fluid>
+                        <Form>
+                            <FormGroup>
+                                <Label for="name">Restaurant Name {' '}</Label>
+                                <Input
                                     type="text"
                                     placeholder=""
                                     name="name"
+                                    id="name"
                                     value={this.state.name}
                                     onChange={this.handleChange}/>
-                            </p>
-                            <p>
-                                Location {' '}
-                                <input
+                            </FormGroup>
+                            <FormGroup>
+                                <Label for="name">Location {' '}</Label>
+                                <Input
                                     type="text"
                                     name="area"
+                                    id="area"
                                     placeholder="five digit zip code"
                                     value={this.state.area}
                                     onChange={this.handleChange}/>
-                            </p>
-                            <p>
-                                Food Type {' '}
-                                <input
-                                    type="text"
+                            </FormGroup>
+                            <FormGroup>
+                                <Label for="foodType">Food Type {' '}</Label>
+                                <Input
+                                    type="select"
                                     name="foodType"
-                                    placeholder="Hunan, Sichuan..."
+                                    id="foodType"
                                     value={this.state.foodType}
-                                    onChange={this.handleChange}/>
-                            </p>
-                        </form>
-                        <button onClick={this.handleSubmit}>Create new restaurant</button>
-                    </div>
+                                    onChange={this.handleChange}>
+                                    <option>Cantonese</option>
+                                    <option>Sichuan</option>
+                                    <option>Hunan</option>
+                                    <option>Fujian</option>
+                                    <option>Jiangsu</option>
+                                    <option>Zhejiang</option>
+                                    <option>Anhui</option>
+                                    <option>Shandong</option>
+                                </Input>
+                            </FormGroup>
+                        </Form>
+                        <Button onClick={this.handleSubmit}>Create new restaurant</Button>
+                    </Container>
                 </div>
             );
         } else {
