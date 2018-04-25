c4495154d4a1cf83c1aee571bf506ebe85cf3915
fayeoyaee <ouyang.f@husky.neu.edu>
2018-04-25 11:25:41 -0700

    try deploy

--- routes/resturantRoute.js
+++ routes/resturantRoute.js
@@ -35,6 +35,7 @@ router.post('/restaurant/new', function (req, res) {
                         }
                     });
                     console.log(newRestaurant)
+
                 });
         } else {
             var newRestaurant = new Restaurant({
